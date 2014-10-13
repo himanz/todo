97319bc66c1fdb4ef8d7259679850ac8ef8c54bb
Jonathan Chang <jonoschang7@gmail.com>
2014-10-12 22:19:47 -0400

    create users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,29 @@
+module Api
+	module V1
+	  class UsersController < ApplicationController
+	  	respond_to :json
+
+	  	def index
+	  		respond_with User.all
+	  	end
+
+	  	def show
+	  		respond_with User.find(params[:id])
+	  	end
+
+	  	def create
+	  		@user = User.create(task_params)
+	  		if @User.save
+	  			respond_with :api, @task
+	  		end
+	  	end
+
+	  	private
+
+	  	def user_params
+	  		params.require(:user).permit(:email, :password, :password_confirmation)
+	  	end
+	  end
+	end
+end
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+	respond_to :html, :json
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   # protect_from_forgery with: :null_session
-  # protect_from_forgery with: :null_session,
-  #     if: Proc.new { |c| c.request.format =~ %r{application/json} }
+  protect_from_forgery with: :null_session,
+      if: Proc.new { |c| c.request.format =~ %r{application/json} }
 end
