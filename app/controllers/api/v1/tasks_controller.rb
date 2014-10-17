10fd42f6d8c3bf223db3df40ee5858e1777f2cb7
Jonathan Chang <jonoschang7@gmail.com>
2014-10-17 17:43:48 -0400

    only authenticated users can see tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,7 +1,7 @@
 module Api
 	module V1
 	  class TasksController < ApplicationController
-	  	# before_action :authenticate_user!
+	  	before_action :authenticate_user!
 	  	respond_to :json
 
 	  	def index
